b9cbd207c24ea977396d5d9337ab40c3b800d6f3
FelipeNapoli <felipenapoli2025@gmail.com>
2026-09-09 10:23:01 -0300

    feat: Funcionalidades do SlideShow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,34 @@
+let imagens = [
+    "/src/assets/carro-azul.jpeg", 
+    "/src/assets/carro-chapeu.jpeg", 
+    "/src/assets/carro-mercedes.jpeg"
+]
+
+// Posição que vai iniciar as imagens
+let index= 0 
+
+// Tempo para trocar as imagens
+let tempo = 3000 // 3 segundos
+
+// Funjção do slideshow
+function Slideshow(){
+    // DOM - Pega o id e passa o caminho das imagens
+    document.getElementById("imgBanner").src = imagens[index]    
+
+    // Incremento
+    index++
+
+    // Estrutura condicional IF
+    if(index == imagens.length){
+        index = 0
+    }
+
+    // callback função que chama outra função
+    setTimeout('Slideshow()', tempo)
+}
+
+Slideshow()
+
 const menuIcone = document.getElementById("menu-icon");
 const navMenu = document.getElementById("nav-menu");
 
